84f405a50e91150be17218edc3232432e6f24986
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-08-09 10:58:56 +0000

    通知画面作成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,13 @@
 class NotificationsController < ApplicationController
  def index
     #current_userの投稿に紐づいた通知一覧
-    @notifications = current_user.passive_notifications
-  
-    #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知の
-    
+    @notifications = current_user.passive_notifications.where.not(visiter_id: current_user.id)
+
+    #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+
  end
 
   def destroy_all
