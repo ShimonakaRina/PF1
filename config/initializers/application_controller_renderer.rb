180a70681d32749b825f6994c403e466113f9027
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-08-16 11:03:38 +0000

    Refile

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'efb020a9cf4cf3ead70a5df07c292274d704b418b584ac94fb77071ab67f1e9446df3e183c06b16b3d519f5445fc871bd9f303ac7530564db3618725391e6b3e'
